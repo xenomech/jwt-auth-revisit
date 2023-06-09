b3e6e1fd365ada8e7d9a44017030d87f3a7d4ee3
xenomech <gokulsmenon227@gmail.com>
2023-06-09 13:08:34 +0530

    connect MongoDB

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL);
+        console.log(`MongoDB_connected ${conn.connection.host}`)
+    } catch (error) {
+        console.error(error);
+        process.exit(1)
+    }
+}
+
+export default connectDB;
